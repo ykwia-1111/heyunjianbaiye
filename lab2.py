f47b5984344dd6b52b6f327d2de7345a9735b8cd
ykwia-1111 <1341097285@qq.com>
2025-09-15 13:37:15 +0800

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -4,11 +4,11 @@ from sklearn.datasets import make_blobs
 # Creating a sample dataset with 4 clusters
 X, y = make_blobs(n_samples=100, n_features=3, centers=4)
 # Initializing KMeans with 4 clusters
-kmeans = _________________
+kmeans = __KMeans(n_clusters=4)_______________
 # Fitting with inputs
-kmeans = kmeans.__________(X)
+kmeans = kmeans.fit__________(X)
 # Labelling the clusters
-labels = kmeans.__________(X)
+labels = kmeans.labels___________(X)
 print(labels)
 # Getting the cluster centers
 C = kmeans._______________
